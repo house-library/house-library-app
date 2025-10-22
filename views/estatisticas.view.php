7f215d7a3a6d260b1015300ccb20bbd716a2d3e7
mariajulia1303 <maria.julia.ss03@gmail.com>
2025-10-22 14:42:29 -0300

    feat: finalizar css estatisticas e detalhes do ebook

--- views/estatisticas.view.php
+++ views/estatisticas.view.php
@@ -12,6 +12,7 @@
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
  <link rel="stylesheet" href="/views/partials/styles/footer.css">
 <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js@4.5.0"></script>
     <link rel="stylesheet" href="../views/partials/styles/reset.css">
     <link rel="stylesheet" href="../views/partials/styles/headeradm.css">
     <link rel="stylesheet" href="../views/partials/styles/footer.css">
@@ -24,7 +25,7 @@
   <h1>Painel de estatísticas</h1>
 </main>
 
- <div id="myPlot" style="width:100%;max-width:700px"></div>
+ <div id="myPlot" style="width:80%;max-width:650px"></div>
 
 <script>
 const xArray = [40,50,60,70,80,90,100,110,120,130,140,150];
@@ -48,11 +49,85 @@ const layout = {
 Plotly.newPlot("myPlot", data, layout);
 </script>
 
-<h2>Mais vendidos</h2>
+<canvas id="myChart" style="width:75%;max-width:360px"></canvas>
 
-<?php require 'partials/footer.php'; ?>
+<script>
+const xValues = ["Se ame", "Nossa química", "Vida secreta", "Era uma vez", "Espaço sideral"];
+const yValues = [55, 49, 44, 24, 15];
+const barColors = ["red", "green","blue","orange","brown"];
+
+const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    plugins: {
+      legend: {display: false},
+      title: {
+        display: true,
+        text: "Ebooks mais vendidos",
+        font: {size: 16}
+      }
+    }
+  }
+});
+</script>
+
+<h2>Mais vendidos</h2>
+          <div class="book-item">
+          <img src="/public/assets/capas-pi/ficcçao cientifica/2099.png" alt="2099">
+          <div class="book-info">
+               <h3>2099</h3>
+               <p>8 capitulos</p>
+               <p>Ficção cientifica</p>
+          </div>
+          </div>
+
+          <div class="book-item">
+          <img src="/public/assets/capas-pi/ficcçao cientifica/2099.png" alt="2099">
+          <div class="book-info">
+               <h3>2099</h3>
+               <p>8 capitulos</p>
+               <p>Ficção cientifica</p>
+          </div>
+        </div>
+
+          <div class="book-item">
+          <img src="/public/assets/capas-pi/ficcçao cientifica/2099.png" alt="2099">
+          <div class="book-info">
+               <h3>2099</h3>
+               <p>8 capitulos</p>
+               <p>Ficção cientifica</p>
+          </div>
+          </div>
+
+          <div class="book-item">
+          <img src="/public/assets/capas-pi/ficcçao cientifica/2099.png" alt="2099">
+          <div class="book-info">
+               <h3>2099</h3>
+               <p>8 capitulos</p>
+               <p>Ficção cientifica</p>
+          </div>
+          </div>
+
+          <div class="book-item">
+          <img src="/public/assets/capas-pi/ficcçao cientifica/2099.png" alt="2099">
+          <div class="book-info">
+               <h3>2099</h3>
+               <p>8 capitulos</p>
+               <p>Ficção cientifica</p>
+          </div>
+          </div>
 
 </body>
 </html>
 
+<?php require 'partials/footer.php'; ?>
     
