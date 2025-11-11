f2d9569ccf9d1cc1d1451327b8ce4d51a5456e28
mariajulia1303 <maria.julia.ss03@gmail.com>
2025-11-11 03:19:49 -0300

    feat: login e recsenha css

--- App/views/recsenha.view.php
+++ App/views/recsenha.view.php
@@ -4,26 +4,26 @@
 
     <main class="main-login">
        
-        <figure class="logo-container">
-      <img class="logo-login" src="../public/assets/imgs/logo.svg" alt="Logo da Marca">
-      <figcaption class="brand-name">House Library</figcaption>
-    </figure>
-        <div class="card-login">
-           
-            <div class="textfield">
-                <label for="usuario">Nova senha</label>
-                <input type="password" name="email" placeholder="Nova senha">
-            </div>
-            <div class="textfield">
-                <label for="senha">Digite novamente</label>
-                <input type="repeat-password" id="repeat-password" name="repeat-password" placeholder="Digite novamente">
-            </div>
-             
-            <button class="password-confirm">Confirmar</button>
-           
+       <body>
+    <div class="main-login">
+        
+        
+        
+        <div class="right-login">
+            <div class="card-login">
+                <img src="assets/imgs/logo.svg" alt="House Library">
+          <h1>House Library</h1>
+                <div class="textfield">
+                  <label for="senha">Nova senha</label>
+                  <input type="text" name="senha" placeholder="Nova senha">
+                </div>
+                <div class="textfield">
+                    <label for="senha">Digite novamente</label>
+                    <input type"password" name="senha" placeholder="Digite novamente">
+                </div>
+                <button class="btn-login">Entrar</button>
+            
+          </div>
         </div>
-    
-    
-    </main>
-
-    <?php loadPartial('footer'); ?>    
+    </div>
+</body>
