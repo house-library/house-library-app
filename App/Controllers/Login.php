a6ccc9e04ca3eeff2c5ee84b0611b9322427bf4c
luiz <guga5378@gmail.com>
2025-11-14 11:45:30 -0300

    Refactor: login.php

--- App/Controllers/Login.php
+++ App/Controllers/Login.php
@@ -2,6 +2,7 @@
 // Caminho: App/Controllers/CarrinhoController.php
 
 namespace App\Controllers;
+use Framework\Database;
 
 class Login
 {
@@ -14,4 +15,30 @@ class Login
 
         loadView('login', $data);
     }
+}
+
+// Necessita de atualização
+try{
+    $nome = $_POST['nome'];
+    $senha = $_POST['senha'];
+ 
+    $sql = "SELECT * FROM CLIENTES WHERE nome_cliente = :nome AND senha_cliente = :senha ";
+    $query = $pdo->prepare($sql);
+    $query->bindParam(':nome',$nome,PDO::PARAM_STR);
+    $query->bindParam(':senha',$senha,PDO::PARAM_STR);
+    $query->execute();
+ 
+    if($query->rowCount()>0){
+        $_SESSION['admin_logado'] = true;
+        header('Location:painel_admin.php');
+        exit;
+    }else {
+        $_SESSION['mensagem_erro']= "Nome de administrador ou senha incorretos";
+        header('Location:login.php');
+        exit;
+    }
+ 
+}catch(Exception $e){
+    $_SESSION['mensagem_erro'] = "Erro de conexão " . $e->getMessage();
+    header('Location:login.php?erro');
 }
