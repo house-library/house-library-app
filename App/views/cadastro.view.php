e9ac691d204d8f1494ac1456de18b8eeae11952e
mariajulia1303 <maria.julia.ss03@gmail.com>
2025-11-14 16:40:14 -0300

    feat: cadastro css

--- App/views/cadastro.view.php
+++ App/views/cadastro.view.php
@@ -3,52 +3,39 @@
 <?php include ("vlibras.html");?>
 
 
-    <main class="main-login">
-
-      <figure class="logo-container">
-        <img class="logo-login" src="/assets/imgs/logo.svg" alt="Logo da House Library">
-        <figcaption class="brand-name">House Library</figcaption>
-      </figure>
-      
-      <form class="card-login">
-    
+    <body>
+    <div class="main-login">
         
-           
-             <div class="textfield">
-                <label for="name">Nome</label>
-                <input type="text" id="name" name="name" placeholder="Nome do Usuário">
-            </div>
-
-            <div class="textfield">
-                <label for="cpf">CPF</label>
-                <input type="text" id="cpf" inputmode="numeric" name="cpf" placeholder="CPF">
-            </div>
-
-            <div class="textfield">
-                <label for="email">Email</label>
-                <input type="email" id="email" name="email" placeholder="Email">
-            </div>
-
-            <div class="textfield">
-                <label for="password">Senha</label>
-                <input type="password" id="password" name="password" placeholder="Senha">
-            </div>
-
-             <div class="textfield">
-                <label for="repeat-password">Repetir senha</label>
-                <input type="password" id="repeat-password" name="repeat-password" placeholder="Repetir senha">
-            </div>
-         
-            
-            <button class="btn-register">Criar conta</button>
-            
-        
-        </form>
-    
-    
-    </main>
-    
-<?php loadPartial('footer'); ?>    
-
-
-    
+        <div class="left-login">
+            <h1>Cadastre-se na House Library!</h1>
+            <img src="assets/imgs/cadastro.svg" class="left-login-image" alt="Animação login">
+        </div>
+        <div class="right-login">
+            <div class="card-login">
+                <img src="assets/imgs/logo.svg" alt="House Library">
+          <h1>House Library</h1>
+                <div class="textfield">
+                  <label for="name">Nome</label>
+                  <input type="text" name="name" placeholder="Nome de usuário">
+                </div>
+                <div class="textfield">
+                    <label for="cpf">CPF</label>
+                    <input type="text" id="cpf" inputmode="numeric" name="cpf" placeholder="CPF">
+                </div>
+                <div class="textfield">
+                    <label for="email">Email</label>
+                    <input type="email" id="email" name="email" placeholder="Email">
+                </div>
+                <div class="textfield">
+                    <label for="senha">Senha</label>
+                    <input type="password" id="repeat-password" name="repeat-password" placeholder="Senha">
+                </div>
+                <div class="textfield">
+                    <label for="senha">Repetir senha</label>
+                    <input type="password" id="repeat-password" name="repeat-password" placeholder="Repetir senha">
+                </div>
+                <button class="btn-login">Entrar</button>
+          </div>
+        </div>
+    </div>
+</body>
